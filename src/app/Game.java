c962d16e5ea86170020c4be52d385633ed941e14
brankomilovanovic2000 <brankomilovanovic2000@gmail.com>
2021-11-07 19:05:16 +0100

    aa

--- src/app/Game.java
+++ src/app/Game.java
@@ -63,22 +63,22 @@ public class Game extends Gui {
 					 int randomNumber = random.nextInt(6) + 1;
 					 System.out.println("Red player rolls dice! Number: " + randomNumber);
 					 if(randomNumber == 1) {
-						 buttonDice.setText("<html>•");
+						 buttonDice.setText("<html>*");
 					 }
 					 else if(randomNumber == 2) {
-						 buttonDice.setText("<html>•<br><br>⠀⠀⠀•<br>");
+						 buttonDice.setText("<html>*<br><br>⠀⠀⠀*<br>");
 					 }
 					 else if(randomNumber == 3) {
-						 buttonDice.setText("<html>•⠀⠀<br>  •<br>⠀⠀⠀•<br>");
+						 buttonDice.setText("<html>*⠀⠀<br>  •<br>⠀⠀⠀•<br>");
 					 }
 					 else if(randomNumber == 4) {
-						 buttonDice.setText("<html>•⠀⠀•<br><br>•⠀⠀•<br>");
+						 buttonDice.setText("<html>*⠀⠀•<br><br>•⠀⠀•<br>");
 					 }
 					 else if(randomNumber == 5) {
-						 buttonDice.setText("<html>•⠀⠀•<br>  •<br>•⠀⠀•<br>");
+						 buttonDice.setText("<html>*⠀⠀•<br>  •<br>•⠀⠀•<br>");
 					 }
 					 else if(randomNumber == 6) {
-						 buttonDice.setText("<html>• • •<br>• • •<br>• • •<br>");
+						 buttonDice.setText("<html>* • •<br>• • •<br>• • •<br>");
 					 }
 					 playerRed.setPlayerRollsDiceNumber(randomNumber);
 					 
